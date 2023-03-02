a3197e5c56c384f55d623222a7bde66c97a48e8b
[dsnaydina] <[dsnaydina@gmail.com]>
2023-03-02 18:04:03 +0100

    update

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const createEsbuildPlugin =
+  require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
+const addCucumberPreprocessorPlugin =
+  require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
 
 module.exports = defineConfig({
   projectId: "teyzqz",
@@ -6,7 +11,14 @@ module.exports = defineConfig({
     baseUrl: "https://staging.lpitko.ru/",
     testIsolation: false,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      const bundler = createBundler({
+        plugins: [createEsbuildPlugin(config)],
+      });
+
+      on("file:preprocessor", bundler);
+      addCucumberPreprocessorPlugin(on, config);
+
+      return config;
     },
   },
 });
